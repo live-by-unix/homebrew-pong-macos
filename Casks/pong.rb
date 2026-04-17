918538a7fd6ad7f2a93e307914678f5c8440b39c
live-by-unix <abhayscontact@gmail.com>
2026-04-16 17:32:00 -0700

    Create pong.rb

---
+++ Casks/pong.rb
@@ -0,0 +1,17 @@
+cask "pong" do
+  version "0.9.0-MAC"
+  sha256 "cac00a0c69132237ad3920849ef4786592b64358876cbfa59c449e965f66e30e"
+
+  url "https://github.com/TheLinuxGroup/Pong/releases/download/v#{version}/Pong-macos.tar.gz"
+  name "Pong"
+  desc "Pong game in GDScript for macOS"
+  homepage "https://github.com/TheLinuxGroup/Pong"
+
+  # Homebrew will unpack the .tar.gz and look for this .app folder inside
+  app "Pong.app"
+
+  zap trash: [
+    "~/Library/Application Support/Godot/app_userdata/Pong",
+    "~/Library/Saved Application State/org.godotengine.pong.savedState",
+  ]
+end
